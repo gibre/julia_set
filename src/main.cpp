47b3d9bbfe32e21a952c894c114742b384194534
Gil Brechbuhler <gibre12@gmail.com>
2013-09-26 12:34:02 +0200

    Just testing a little bit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 #include <iostream>
-
-using namespace std;
 
 int main()
 {
-	cout << "dsqjdsqjd" << endl;
+	sf::Window *window;
+	window = new sf::Window(sf::VideoMode(640, 480), "SFML works!");
+	window->setVerticalSyncEnabled(true);
+
+	sf::Clock clock;
+	while (window->isOpen())
+	{
+		sf::Event event;
+		while (window->pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			       window->close();
+		}
+
+		window->display();
+
+		sf::Time frameTime = clock.restart();
+		float fps = 1.0f / frameTime.asSeconds();
+		std::cout << fps << std::endl;
+	}
+
+	delete(window);
+
 	return 0;
 }
 
